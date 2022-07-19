a495e2b374c13033bf462e08b85b8d45d7220c60
Nada Shawky <shawkyn12@gmail.com>
2022-07-19 03:27:09 +0200

    dark_light_mode

---
+++ demo.js
@@ -0,0 +1,51 @@
+// left ---------> headline
+let sahm = document.getElementById("sahm")
+let headline = document.getElementById("headline")
+let ul = document.getElementById("ul")
+
+headline.onclick = function(){
+    if(sahm.getAttribute('class')=="fa-solid fa-angle-down"){
+            sahm.setAttribute('class' , 'fa-solid fa-angle-up');
+            ul.style.display="block";
+
+    }
+    else if(sahm.getAttribute('class')=="fa-solid fa-angle-up"){
+            sahm.setAttribute("class" , "fa-solid fa-angle-down");
+            ul.style.display="none";
+        
+    }
+    
+}
+
+// ------> dashboard
+let sahm2 = document.getElementById("sahm2")
+let Dashboard = document.getElementById("Dashboard")
+let ul2 = document.getElementById("ul2")
+
+Dashboard.onclick = function(){
+    if(sahm2.getAttribute('class')=="fa-solid fa-angle-down"){
+            sahm2.setAttribute('class' , 'fa-solid fa-angle-up');
+            ul2.style.display="block";
+
+    }
+    else if(sahm2.getAttribute('class')=="fa-solid fa-angle-up"){
+            sahm2.setAttribute("class" , "fa-solid fa-angle-down");
+            ul2.style.display="none";
+        
+    }
+    
+}
+
+// dark mood
+let icon = document.getElementById("moon");
+let bod = document.getElementById("body");
+// <i class="fa-solid fa-sun"></i> 
+// <i class="fa-solid fa-moon"></i>
+icon.onclick= function(){
+    bod.classList.toggle("dark")
+    if(icon.getAttribute("class")=="fa-solid fa-moon"){
+        icon.setAttribute("class", "fa-solid fa-sun")
+    }else{
+        icon.setAttribute("class", "fa-solid fa-moon")
+    }
+}
